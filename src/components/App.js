98f64e12ac755b4090a7a4533e4e8c958307c357
Furmanov19 <artem.furmanov2014@mail.ru>
2019-06-27 11:49:47 +0300

    feat: add theme
    add default theme for styled components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import Atom from 'components/atoms/Atom';
+import theme from './themes/default';
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Atom>Hello world</Atom>
+    </ThemeProvider>
+  );
 }
 
 export default App;
